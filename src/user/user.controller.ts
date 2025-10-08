985afaf9fb739c971a1fbd433aff32ee735f8706
islammusicscv <islam.music@scv.si>
2025-10-08 09:29:16 +0200

    delete, patch user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 import {UserService} from "./user.service";
 import {CreateUserDto} from "./create-user.dto";
 import {User} from "./user.entity";
+import {UpdateUserDto} from "./update-user.dto";
 
 @Controller('users')
 export class UserController {
@@ -20,5 +21,15 @@ export class UserController {
     @Get(':id')
     findById(@Param('id') id:string) {
         return this.userService.findById(+id);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:string) {
+        return this.userService.delete(+id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id:string,@Body() updateUserDto: UpdateUserDto) {
+        return this.userService.update(+id, updateUserDto);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import {Repository} from "typeorm";
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {DeleteResult, Repository} from "typeorm";
 import {User} from "./user.entity";
 import {InjectRepository} from "@nestjs/typeorm";
 import {CreateUserDto} from "./create-user.dto";
+import {UpdateUserDto} from "./update-user.dto";
+import {NotFoundError} from "rxjs";
 
 @Injectable()
 export class UserService {
@@ -21,5 +23,18 @@ export class UserService {
 
     async findById(id: number): Promise<User | null> {
         return await this.userRepository.findOne({ where: {id} });
+    }
+
+    delete(id: number): Promise<DeleteResult> {
+        return this.userRepository.delete(id);
+    }
+
+    async update(id: number, updateUserDto: UpdateUserDto): Promise<User | null> {
+        const user = await this.findById(id);
+        if(!user) {
+            throw new NotFoundException('User does not exist!!');
+        }
+        await this.userRepository.update(id,updateUserDto);
+        return await this.findById(id);
     }
 }
