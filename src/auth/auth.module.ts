4b98b71c3f2e35e4b96f86d51520757aaa75f7e0
islammusicscv <islam.music@scv.si>
2025-11-05 09:19:10 +0100

    dodan jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import {UserModule} from "../user/user.module";
+import {JwtModule} from "@nestjs/jwt";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [UserModule],
+  imports: [
+      UserModule,
+      ConfigModule.forRoot({isGlobal: true}),
+      JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,22 @@
 import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {UserService} from "../user/user.service";
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService) {}
 
     async signIn(username: string, pass: string): Promise<any> {
         const user = await this.userService.findOne(username);
-        if (user?.password !== pass) {
+        if (!user || !(await bcrypt.compare(pass, user.password))) {
             throw new UnauthorizedException();
         }
-        const { password, ...result } = user;
-        return result;
+        const payload = { sub: user.id, username: user.email };
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        };
     }
 }
